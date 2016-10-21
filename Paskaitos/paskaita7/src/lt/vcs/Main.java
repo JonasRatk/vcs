cc6fb6b577898c75cb5218e2b61a60db337b7526
DESKTOP-1R580GC\Jon <anoniminis00@gmail.com>
2016-10-21 22:11:19 +0300

    7paskaitos "pabaiga" Veliau papildysiu

--- Paskaitos/paskaita7/src/lt/vcs/Main.java
+++ Paskaitos/paskaita7/src/lt/vcs/Main.java
@@ -4,25 +4,44 @@ import sun.java2d.pipe.BufferedRenderPipe;
 
 import java.io.*;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         File file = new File("D:\\Temp\\failas.txt");
         VcsUtils.println(file.toString());
 
+        PvzKlase pvz = new PvzKlase<File>(file);
+        Object kazkas = pvz.getReiksme();
+        if (kazkas instanceof File ){
+            File tikraiFailas
+        }
 
-        BufferedWriter bw = VcsUtils.newWriter(file.toString());
-        bw.append(VcsUtils.NEW_LINE + "ketvirta");
-        bw.flush();
-        bw.close();
 
+        List<Integer> listas = new ArrayList<>();
+        for (int i - 1; i < 6, i++){
+            listas.add(i);
+        }
 
+//            BufferedWriter bw = VcsUtils.newWriter(file.toString());
+//            bw.append(VcsUtils.NEW_LINE + "ketvirta");
+//            bw.flush();
+//            bw.close();
+//
+        try {
         BufferedReader br = VcsUtils.newReader(file.toString());
         String line;
         while ((line = br.readLine()) != null) {
             VcsUtils.println(line);
         }
-        bw.close();
+        br.close();
+        } catch(IOException ex){
+            VcsUtils.println(ex.getMessage());
+            throw new RuntimeException("NEPASISEKE ", ex);
+        } catch(Exception ex){
+
+        }
     }
 }
---
+++ Paskaitos/paskaita7/src/lt/vcs/PvzKlase.java
@@ -0,0 +1,14 @@
+package lt.vcs;
+
+/**
+ * Created by Jon on 2016-10-21.
+ */
+public class PvzKlase<T> {
+    private T reiksme;
+    public PvzKlase(T arg) {
+        reiksme = arg;
+    }
+    public T getReiksme() {
+        return reiksme;
+    }
+}
